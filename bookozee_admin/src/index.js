e020400687c81b8d6cde5d6910860289b58fcf76
Mohit Patil <97459016+mohit7067@users.noreply.github.com>
2023-04-05 15:31:47 +0530

    backed url changed

--- bookozee_admin/src/index.js
+++ bookozee_admin/src/index.js
@@ -4,7 +4,7 @@ import App from "./App";
 import axios from "axios";
 import { DarkModeContextProvider } from "./context/darkModeContext";
 import { AuthContextProvider } from "./context/AuthContext";
-axios.defaults.baseURL = "https://bookozee.vercel.app/api";
+axios.defaults.baseURL = "https://bookozee-backend.vercel.app/api";
 // axios.defaults.baseURL = "http://localhost:8080/api";
 axios.defaults.withCredentials = true;
 ReactDOM.render(
